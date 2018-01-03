6e316b4ee72d95a4570c14690e0849041490b406
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 15:13:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,11 @@
 class Application
 
-  @@items = []
+  @@items = [Item.new("Apples",5.23), Item.new("Oranges",2.43)]
+
+def call(env)
+  resp = Rack::Response.new
+  req = Rack::Request.new(env)
+
+  if req.path=="items/"
+  
+end
