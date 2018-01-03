181b058745f69f920b509ae11b43e432eb496b71
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 15:20:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
 
       resp.write "#{item.price}"
-    elsif
+    elsif req.path!="/items/"
       resp.write "Route not found"
       resp.status = 404
     else
